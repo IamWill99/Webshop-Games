285f83ceb73b9b9670fe61fd4ced1bf52c794fdf
Mary Anyanwu <maria.anyanwu@hva.nl>
2024-06-12 16:39:25 +0200

    Alle OrderItems werken nu weer

--- src/api/src/services/OrderItemService.ts
+++ src/api/src/services/OrderItemService.ts
@@ -1,25 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { OrderItem } from "@shared/types";
 import { getConnection, queryDatabase } from "./DatabaseService";
 
 /**
  * Handles order item related functionality
  */
- export class OrderItemService {
+export class OrderItemService {
     /**
      * Get all order items
      * 
      * @returns A list of all order items when successful, otherwise `undefined`.
      */
-    public async getAll(): Promise<OrderItem[] | undefined> {
-      
+    public async getAll(sortOn: string): Promise<OrderItem[] | undefined> {
+        console.log("sortOn", sortOn);
+
         const connection: any = await getConnection();
 
-        const response: any[] = await queryDatabase(connection, "SELECT * FROM product;");
+        let query: string = "SELECT * FROM product;";
 
-      
+        switch (sortOn) {
+            case "ASC": {
+                query = "SELECT * FROM product ORDER BY name ASC; ";
+                break;
+            }
+
+            case "DESC": {
+                query = "SELECT * FROM product ORDER BY name DESC; ";
+                break;
+            }
+
+        }
+
+
+        const response: any[] = await queryDatabase(connection, query, sortOn);
+
+
 
         console.log(response);
-    
+
         return response;
     }
 }
--- src/web/src/services/OrderItemService.ts
+++ src/web/src/services/OrderItemService.ts
@@ -1,4 +1,5 @@
 import { OrderItem } from "@shared/types";
+
 /**
  * Handles order item related functionality
  */
@@ -9,8 +10,9 @@ export class OrderItemService {
      * 
      * @returns A list of all order items when successful, otherwise `undefined`.
      */
-    public async getAll(): Promise<OrderItem[] | undefined> {
-        const response: Response = await fetch(`${viteConfiguration.API_URL}orderItems`, {
+        public async getAll(sortingOrder: string): Promise<OrderItem[] | undefined> {
+            
+        const response: Response = await fetch(`${ viteConfiguration.API_URL }orderItems?sort= ${sortingOrder }`, {
             method: "get",
         });
 
